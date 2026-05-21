e6383f0b5190e72d7f55d8294721afac55c52afe
mdforhad80 <mdforhad@kakao.com>
2026-05-21 12:50:48 +0600

    Update router.js

--- assets/js/router.js
+++ assets/js/router.js
@@ -1,14 +1,4 @@
-export function getParams() {
-  return new URLSearchParams(window.location.search);
-}
-
-export function navigateTo(url) {
-  window.location.href = url;
-}
-
-export function buildWatchUrl(id, type, options = {}) {
-  if (type === 'movie') {
-    return `watch.html?id=${id}&type=movie&server=${options.server || 'vidplus'}`;
-  }
-  return `watch.html?id=${id}&type=tv&season=${options.season || 1}&episode=${options.episode || 1}&server=${options.server || 'vidplus'}`;
+// router.js – Simple utility to parse query params
+function getQueryParams() {
+  return Object.fromEntries(new URLSearchParams(window.location.search).entries());
 }
